2e5b267663c7bffcba7f918cfecdbe33dade2585
attila <atti80@gmail.com>
2025-07-30 18:01:38 +0200

    api delete

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { ENV } from './config/env.js';
 import { db } from './config/db.js';
 import { favoritesTable } from './db/schema.js';
+import { eq, and } from 'drizzle-orm';
 
 const app = express();
 const port = ENV.PORT;
@@ -38,6 +39,21 @@ app.post("/api/favorites", async (req, res) => {
         res.status(500).json({ error: "Something went wrong." });
     }
 
+});
+
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+    try {
+        const { userId, recipeId } = req.params;
+
+        await db.delete(favoritesTable).where(
+            and(eq(favoritesTable.userId, userId), eq(favoritesTable.recipeId, parseInt(recipeId)))
+        );
+
+        res.status(200).json({ message: "Favorite removed successfully" });
+    } catch (error) {
+        console.error("Error removing favorite!", error);
+        res.status(500).json({ error: "Something went wrong." });
+    }
 });
 
 app.listen(port, () => {
